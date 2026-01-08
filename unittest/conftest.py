c9fe9a09f97b2a194ad898981978883763e1bb65
dstreppa <davide.strepparava@supsi.ch>
2026-01-08 13:57:36 +0100

    Added conftest.py

---
+++ unittest/conftest.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(TESTS_DIR)
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
